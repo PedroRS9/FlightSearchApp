677e97adb9f5751b4673465e98479e249f8abff4
PedroRS9 <pedroglv98@gmail.com>
2023-11-05 18:09:11 +0000

    Favorite Dao created

---
+++ app/src/main/java/com/example/flightsearchapp/data/FavoriteDao.kt
@@ -0,0 +1,11 @@
+package com.example.flightsearchapp.data
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteDao {
+    @Query("SELECT * FROM favorite")
+    fun getAll(): Flow<List<Favorite>>
+}
